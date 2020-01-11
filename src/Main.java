a78a1bca1fa881cca3f18b0912d039fdcd6c7045
neeekha <annika.e@gmx.net>
2020-01-11 15:19:45 +0100

    To Dos ergänzt

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ import java.util.List;
 public class Main {
 	public static void main(String[] args) {
 
-		//Hier Login erfassen!
+		//Hier Login, Konto erfassen und Logout erstellen!
 		
 		
 		// Kunden erfassen
