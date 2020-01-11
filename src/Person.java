db38cd41fd17356a77bb1a985f16d1dc26afc044
saliami <amina8132@hotmail.com>
2020-01-11 17:46:29 +0100

    Methode Suchen erstellt

--- src/Person.java
+++ src/Person.java
@@ -1,7 +1,10 @@
 import java.io.Serializable;
 import java.util.Scanner;
 
+import veranstalter.Aktivitaet;
+import veranstalter.Veranstalter;
 import veranstalter.VeranstalterService;
+import veranstalter.Veranstaltung;
 
 public class Person implements Serializable {
 
@@ -42,21 +45,80 @@ public class Person implements Serializable {
 		System.out.println("Um eine Veranstaltung zu suchen, wählen Sie 3.");
 		Scanner scan = new Scanner(System.in);
 		int auswahl1 = scan.nextInt();
+		scan.close();
 
 		if (auswahl1 == 1) {
+			// Alle Veranstalter werden angegeben
 			System.out.println("Wählen Sie einen Veranstalter aus:");
 			for (int i = 0; i < service.getVeranstalter().size(); i++) {
-				System.out.println(i +". "+service.getVeranstalter().get(i).getVeranstalter());
+				System.out.println(i + " ist " + service.getVeranstalter().get(i).getVeranstalter());
 			}
-			
-		} else if (auswahl1 == 2) {
-			System.out.println(service.getAktivitaet());
+			// Der gewünschte Veranstalter wird angegeben
+			Scanner scan2 = new Scanner(System.in);
+			int auswahlVeranstalter = scan.nextInt();
+			scan2.close();
+			// Alle Aktivitäten des oben gewünschten Veranstalters werden angegeben
+			Veranstalter veranstalter = service.getVeranstalter().get(auswahlVeranstalter);
 			System.out.println("Wählen Sie eine Aktivität aus:");
-			
-		} else if (auswahl1 == 3) {
-			System.out.println(service.getVeranstaltung());
+			for (int i = 0; i < veranstalter.getAktivitaeten().size(); i++) {
+				System.out.println(i + " ist " + veranstalter.getAktivitaeten().get(i).getBeschrieb());
+			}
+			// Die gewünschte Aktivität wird angegeben
+			Scanner scan3 = new Scanner(System.in);
+			int auswahlAktivitaet = scan.nextInt();
+			scan3.close();
+			// Alle Veranstaltungen der oben gewünschten Aktivität werden angegeben
+			Aktivitaet aktivitaet = service.getAktivitaet().get(auswahlAktivitaet);
 			System.out.println("Wählen Sie eine Veranstaltung aus:");
-			
+			for (int i = 0; i < aktivitaet.getVeranstaltungen().size(); i++) {
+				System.out.println(i + " ist " + aktivitaet.getVeranstaltungen().get(i).getDatum());
+			}
+			// Die gewünschte Veranstaltung wird angegeben
+			Scanner scan4 = new Scanner(System.in);
+			int auswahlVeranstaltung = scan.nextInt();
+			scan4.close();
+			Veranstaltung veranstaltung = service.getVeranstaltung().get(auswahlVeranstaltung);
+
+			// Die gewählten Optionen werden ausgegeben
+			System.out.println("Ihre Auswahl, Veranstalter: " + veranstalter + "/nAktivität: " + aktivitaet
+					+ "/Veranstaltung: " + veranstaltung);
+
+		} else if (auswahl1 == 2) {
+			// Alle Aktivitäten werden ausgegeben
+			System.out.println("Wählen Sie eine Aktivität aus:");
+			for (int i = 0; i < service.getAktivitaet().size(); i++) {
+				System.out.println(i + " ist " + service.getAktivitaet().get(i).getBeschrieb());
+			}
+			// Die gewünschte Aktivität kann gewählt werden
+			Scanner scan5 = new Scanner(System.in);
+			int auswahlAktivitaet = scan.nextInt();
+			scan5.close();
+			// Alle Veranstaltungen werden angegeben von der obigen Aktivität
+			Aktivitaet aktivitaet = service.getAktivitaet().get(auswahlAktivitaet);
+			System.out.println("Wählen Sie eine Veranstaltung aus:");
+			for (int i = 0; i < aktivitaet.getVeranstaltungen().size(); i++) {
+				System.out.println(i + " ist " + aktivitaet.getVeranstaltungen().get(i).getDatum());
+			}
+			Scanner scan6 = new Scanner(System.in);
+			int auswahlVeranstaltung = scan.nextInt();
+			scan6.close();
+			Veranstaltung veranstaltung = service.getVeranstaltung().get(auswahlVeranstaltung);
+
+			System.out.println("Ihre Auswahl, Veranstalter: " + aktivitaet.getVeranstalter() + "/nAktivität: "
+					+ aktivitaet + "/Veranstaltung: " + veranstaltung);
+
+		} else if (auswahl1 == 3) {
+			System.out.println("Wählen Sie eine Veranstaltung aus:");
+			for (int i = 0; i < service.getVeranstaltung().size(); i++) {
+				System.out.println(i + " ist " + service.getVeranstaltung().get(i).getDatum());
+			}
+			Scanner scan7 = new Scanner(System.in);
+			int auswahlVeranstaltung = scan.nextInt();
+			scan7.close();
+			Veranstaltung veranstaltung = service.getVeranstaltung().get(auswahlVeranstaltung);
+
+			System.out.println("Ihre Auswahl, Veranstalter: " + veranstaltung.getAktivitaet().getVeranstalter()
+					+ "/nAktivität: " + veranstaltung.getAktivitaet() + "/Veranstaltung: " + veranstaltung);
 		}
 	}
 
