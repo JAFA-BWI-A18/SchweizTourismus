d91b01d8e41b69348019a033afca95cc5863a21a
Piak48 <ivankovic.josip@gmail.com>
2020-01-14 14:12:07 +0100

    ü aus methoden entfernt

--- src/Data.java
+++ src/Data.java
@@ -60,17 +60,17 @@ public class Data {
 		service.veranstaltungErfassen(eintritt, LocalDateTime.of(2020, 07, 9, 14, 00), 100, 50);
 
 		// Bewertung der Veranstaltung Eintritt Tag 1
-		service.bewertungHinzufügen(eintritt, "Geil", 5);
-		service.bewertungHinzufügen(eintritt, "Hammer", 5);
-		service.bewertungHinzufügen(eintritt, "Nice", 4);
+		service.bewertungHinzufuegen(eintritt, "Geil", 5);
+		service.bewertungHinzufuegen(eintritt, "Hammer", 5);
+		service.bewertungHinzufuegen(eintritt, "Nice", 4);
 
 		// Veranstaltung Tag 2 für Eintritt am OAF
 		service.veranstaltungErfassen(eintritt, LocalDateTime.of(2020, 07, 10, 14, 00), 200, 30);
 
 		// Bewertung der Veranstaltung Eintritt Tag 2
-		service.bewertungHinzufügen(eintritt, "Jedes Jahr wieder", 5);
-		service.bewertungHinzufügen(eintritt, "Danke, mega toll", 5);
-		service.bewertungHinzufügen(eintritt, "Schlimm", 1);
+		service.bewertungHinzufuegen(eintritt, "Jedes Jahr wieder", 5);
+		service.bewertungHinzufuegen(eintritt, "Danke, mega toll", 5);
+		service.bewertungHinzufuegen(eintritt, "Schlimm", 1);
 
 		// Aktivität Übernachtung für OAF
 		Aktivitaet uebernachtung = service.aktivitaetErfassen(oaf, "Outdoor", "Frauenfeld", 8500, "Camping Upgrade");
@@ -79,16 +79,16 @@ public class Data {
 		service.veranstaltungErfassen(uebernachtung, LocalDateTime.of(2020, 7, 9, 14, 00), 100, 100);
 
 		// Bewertung der Veranstaltung Übernachtung Nacht1
-		service.bewertungHinzufügen(uebernachtung, "Mega bequem", 5);
-		service.bewertungHinzufügen(uebernachtung, "Schmutzig", 2);
+		service.bewertungHinzufuegen(uebernachtung, "Mega bequem", 5);
+		service.bewertungHinzufuegen(uebernachtung, "Schmutzig", 2);
 
 		// Veranstaltung Nacht 2 für Übernachtung am OAF
 		service.veranstaltungErfassen(uebernachtung, LocalDateTime.of(2020, 7, 10, 14, 00), 50, 50);
 
 		// Bewertung der Veranstaltung Übernachtung Nacht2
-		service.bewertungHinzufügen(uebernachtung, "Toll", 5);
-		service.bewertungHinzufügen(uebernachtung, "Nie wider", 1);
-		service.bewertungHinzufügen(uebernachtung, "Schlimm", 1);
+		service.bewertungHinzufuegen(uebernachtung, "Toll", 5);
+		service.bewertungHinzufuegen(uebernachtung, "Nie wider", 1);
+		service.bewertungHinzufuegen(uebernachtung, "Schlimm", 1);
 
 		// Veranstalter Alpamare
 		Veranstalter alpamare = service.veranstalterErfassen("Alpamare", "bla bla");
@@ -100,13 +100,13 @@ public class Data {
 		service.veranstaltungErfassen(wellness, LocalDateTime.of(2020, 8, 13, 14, 00), 20, 15);
 
 		// Bewertung Tag 1 Wellness im Alpamare
-		service.bewertungHinzufügen(wellness, "Toll, aber..", 4);
+		service.bewertungHinzufuegen(wellness, "Toll, aber..", 4);
 
 		// Veranstaltung Tag 2 für Wellness im Alpamare
 		service.veranstaltungErfassen(wellness, LocalDateTime.of(2020, 7, 10, 14, 00), 20, 20);
 
 		// Bewertung Tag 2 Wellness im Alpamare
-		service.bewertungHinzufügen(wellness, "Super", 5);
+		service.bewertungHinzufuegen(wellness, "Super", 5);
 
 		// Aktivität Rutschbahnen
 		Aktivitaet rutschbahnen = service.aktivitaetErfassen(alpamare, "Indoor", "Freienbach", 8807, "Tolles Rutscherlebnis");
@@ -115,15 +115,15 @@ public class Data {
 		service.veranstaltungErfassen(rutschbahnen, LocalDateTime.of(2020, 7, 22, 10, 00), 15, 20);
 
 		// Bewertung Tag 1 Rutschbahnen im Alpamare
-		service.bewertungHinzufügen(rutschbahnen, "Sensationell", 5);
-		service.bewertungHinzufügen(rutschbahnen, "Schlecht", 1);
+		service.bewertungHinzufuegen(rutschbahnen, "Sensationell", 5);
+		service.bewertungHinzufuegen(rutschbahnen, "Schlecht", 1);
 
 		// Veranstaltung Tag 2 für Rutschbahnen im Alpamare
 		service.veranstaltungErfassen(rutschbahnen, LocalDateTime.of(2020, 10, 28, 10, 00), 17, 15);
 
 		// Bewertung Tag 2 Rutschbahnen im Alpamare
-		service.bewertungHinzufügen(rutschbahnen, "ok", 3);
-		service.bewertungHinzufügen(rutschbahnen, "Nice", 5);
+		service.bewertungHinzufuegen(rutschbahnen, "ok", 3);
+		service.bewertungHinzufuegen(rutschbahnen, "Nice", 5);
 	}
 
 }
