08aa4f1187d342bebbd6559b68561db25cd3086b
Piak48 <ivankovic.josip@gmail.com>
2020-01-14 13:57:33 +0100

    Bewertung bearbeitet

--- src/veranstalter/Bewertung.java
+++ src/veranstalter/Bewertung.java
@@ -1,15 +1,19 @@
 package veranstalter;
 
 public class Bewertung {
+	
+	private static VeranstalterService service = new VeranstalterService();
+	static {
+		Data.basisVeranstaltung(service);
+	}
 
 	private String bewertungText;
 	private int bewertungStern;
 //TODO: Wir brauchen bei Kunde Menüpunkt Bewerten und diese muss alle Zahlungen auflisten, via Zahlungen kann er alle Aktivitäten auflisten und dann ausäwhlen, welche er bewerten will. 
-	
-	public Bewertung(String bewertungText, int bewertungStern) {
-		this.bewertungText = bewertungText;
-		this.bewertungStern = bewertungStern;
-	}
+
+//	public Bewertung(String bewertungText, int bewertungStern) {
+//		this.bewertungText = bewertungText;
+//		this.bewertungStern = bewertungStern;
 
 	public String getBewertungText() {
 		return bewertungText;
@@ -25,6 +29,10 @@ public class Bewertung {
 
 	public void setBewertungStern(int bewertungStern) {
 		this.bewertungStern = bewertungStern;
+	}
+
+	public static void bewertungHinzufuegen(Aktivitaet aktivitaet, String bewertungText, int bewertungStern) {
+	service.bewertungHinzufuegen(aktivitaet, bewertungText, bewertungStern);
 	}
 
 }
